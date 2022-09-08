335d44c4d8d98680ef24f20fd3725769c6bb72b5
big-mond <bigmond@live.com>
2022-09-07 19:03:29 -0500

    commit

--- weather.py
+++ weather.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import requests
 
 API_KEY = "0e5991b6e9322d7a3047ecb27b346bac"
@@ -20,4 +21,28 @@ if response.status_code == 200:
     print("Feels Like:", feels_like, "F")
     print("Min:", temp_min, "F", "Max:", temp_max, "F")
 else:
+=======
+import requests
+
+API_KEY = "0e5991b6e9322d7a3047ecb27b346bac"
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+city = input("Enter a city name: ")
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+response = requests.get(request_url)
+
+if response.status_code == 200:
+    data = response.json()
+    weather = data['weather'][0]['description']
+    temperature = round(data["main"]["temp"] - 273.15, 0) * 2 + 30
+    feels_like = round(data["main"]["feels_like"] - 273.15, 0) * 2 + 30
+    temp_min = round(data["main"]["temp_min"] - 273.15, 0) * 2 + 30
+    temp_max = round(data["main"]["temp_max"] - 273.15, 0) * 2 + 30
+    
+    print("Weather:", weather)
+    print("Temperature:", temperature, "F")
+    print("Feels Like:", feels_like, "F")
+    print("Min:", temp_min, "F", "Max:", temp_max, "F")
+else:
+>>>>>>> 21a0e36 (commit)
     print("An error has occured.")
